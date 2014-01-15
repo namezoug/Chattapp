ef5c65a6cfaaec3e6f00f92f637701e713066b29
CODERLAB <CODERLAB@coderlab.local>
2014-01-15 20:06:28 +0400

    Initial Commit

---
+++ Chattapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chattapp
+//
+//  Created by CODERLAB on 15.01.14.
+//  Copyright (c) 2014 studio76. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ChattappTests/ChattappTests.m
@@ -0,0 +1,34 @@
+//
+//  ChattappTests.m
+//  ChattappTests
+//
+//  Created by CODERLAB on 15.01.14.
+//  Copyright (c) 2014 studio76. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChattappTests : XCTestCase
+
+@end
+
+@implementation ChattappTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
